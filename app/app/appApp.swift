a4e8f508965f4e26d20a370b383242fc6a2a3238
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-02-23 15:36:28 +0700

    :money_with_wings: setup mobile app

---
+++ app/app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Pornpitak Amornlerdlittichai on 23/2/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
